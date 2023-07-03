b884b5aef6e2b2262ef41dca828a4e6c2eec1397
Arash Rajabpour <arashrajabpour01@gmail.com>
2023-07-03 15:02:28 +0330

    [Add] menu scene and move scripts to seperate folders

---
+++ Assets/Scripts/Menu/MenuUIManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuUIManager : MonoBehaviour
+{
+    [SerializeField] private Button playButton;
+    void Start()
+    {
+        playButton.onClick.AddListener(NextScene);
+    }
+
+    void NextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
